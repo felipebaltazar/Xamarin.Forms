5c0711efbb29e5a242dc91001e946f2370bf5fd9
ksemenenko <KSemenenko@users.noreply.github.com>
2019-07-18 15:28:23 +0300

    [Enhancement] Implements CharacterSpacing (#5167)
    - fixes #4855 
    
    * TextElement.LetterSpacingProperty
    
    ButtonRenderer
    
    DatePickerRenderer
    
    EditorRenderer
    
    EntryRenderer.cs
    
    LabelRenderer
    
    PickerRenderer
    
    SearchBarRenderer
    
    TimePickerRenderer
    
    fix renderers
    
    LetterSpacingElement
    
    Extensions ToEm
    
    * iOS
    
    Picker
    
    * UWP
    
    * removal of an unnecessary condition
    
    * Remove ILetterSpacingElement.OnLetterSpacingChanged
    
    Fix for PickerRenderer for Android
    
    * Move LetterSpacing to TextElement
    
    * UWP
    
    * Span
    
    * Span and LetterSpacing
    
    * Tests
    
    * fixes for renderers
    
    * Renderers
    
    * LetterSpacingGallery
    
    * Android fixes
    
    * Extensions
    
    * delete macos code
    
    * iOS and UWP Renderers
    
    * Renderers for iOS
    
    * Picker.Items
    
    * UWP
    
    * fix merge
    
    * Renderers
    
    * iOS renderers
    
    * PickerRenderer.cs
    
    * Material
    
    * Material Renderer
    
    * Picker placeholder
    
    * InvalidateMeasure
    
    * Update Xamarin.Forms.Core/Span.cs
    
    Co-Authored-By: Stephane Delcroix <stephane@delcroix.org>
    
    * Update Xamarin.Forms.Core/ITextElement.cs
    
    Co-Authored-By: Stephane Delcroix <stephane@delcroix.org>
    
    * remove LetterSpacing tests
    
    * style
    
    * Rename LetterSpacing to CharacterSpacing
    
    * Update Xamarin.Forms.Controls/GalleryPages/CharacterSpacingGallery.xaml
    
    Co-Authored-By: Samantha Houts <samhouts@users.noreply.github.com>
    
    * Update Xamarin.Forms.Material.Android/MaterialButtonRenderer.cs
    
    Co-Authored-By: Samantha Houts <samhouts@users.noreply.github.com>
    
    * Update Xamarin.Forms.Material.Android/MaterialButtonRenderer.cs
    
    Co-Authored-By: Samantha Houts <samhouts@users.noreply.github.com>
    
    * Update Xamarin.Forms.Material.Android/MaterialButtonRenderer.cs
    
    Co-Authored-By: Samantha Houts <samhouts@users.noreply.github.com>
    
    * Update Xamarin.Forms.Material.Android/MaterialButtonRenderer.cs
    
    Co-Authored-By: Samantha Houts <samhouts@users.noreply.github.com>
    
    * Update Xamarin.Forms.Material.iOS/MaterialButtonRenderer.cs
    
    Co-Authored-By: Samantha Houts <samhouts@users.noreply.github.com>
    
    * Update Xamarin.Forms.Material.iOS/MaterialPickerRenderer.cs
    
    Co-Authored-By: Samantha Houts <samhouts@users.noreply.github.com>
    
    * Update Xamarin.Forms.Material.iOS/MaterialButtonRenderer.cs
    
    Co-Authored-By: Samantha Houts <samhouts@users.noreply.github.com>
    
    * Update Xamarin.Forms.Material.iOS/MaterialEditorRenderer.cs
    
    Co-Authored-By: Samantha Houts <samhouts@users.noreply.github.com>
    
    * Update Xamarin.Forms.Material.iOS/MaterialButtonRenderer.cs
    
    Co-Authored-By: Samantha Houts <samhouts@users.noreply.github.com>
    
    * Update Xamarin.Forms.Material.iOS/MaterialEditorRenderer.cs
    
    Co-Authored-By: Samantha Houts <samhouts@users.noreply.github.com>
    
    * Update Xamarin.Forms.Material.iOS/MaterialButtonRenderer.cs
    
    Co-Authored-By: Samantha Houts <samhouts@users.noreply.github.com>
    
    * Update Xamarin.Forms.Material.iOS/MaterialButtonRenderer.cs
    
    Co-Authored-By: Samantha Houts <samhouts@users.noreply.github.com>
    
    * SearchHandler
    
    * UAP CharacterSpacing
    
    * Update Xamarin.Forms.Core/Shell/SearchHandler.cs
    
    Co-Authored-By: Stephane Delcroix <stephane@delcroix.org>

--- Xamarin.Forms.Platform.iOS/Renderers/PickerRenderer.cs
+++ Xamarin.Forms.Platform.iOS/Renderers/PickerRenderer.cs
@@ -71,6 +71,7 @@ namespace Xamarin.Forms.Platform.iOS
 							UpdatePickerSelectedIndex(0);
 						UpdatePickerFromModel(s);
 						entry.ResignFirstResponder();
+						UpdateCharacterSpacing();
 					});
 
 					toolbar.SetItems(new[] { spacer, doneButton }, false);
@@ -98,6 +99,7 @@ namespace Xamarin.Forms.Platform.iOS
 				UpdateFont();
 				UpdatePicker();
 				UpdateTextColor();
+				UpdateCharacterSpacing();
 
 				((INotifyCollectionChanged)e.NewElement.Items).CollectionChanged += RowsCollectionChanged;
 			}
@@ -109,13 +111,24 @@ namespace Xamarin.Forms.Platform.iOS
 		{
 			base.OnElementPropertyChanged(sender, e);
 			if (e.PropertyName == Picker.TitleProperty.PropertyName || e.PropertyName == Picker.TitleColorProperty.PropertyName)
+			{
 				UpdatePicker();
+				UpdateCharacterSpacing();
+			}
 			else if (e.PropertyName == Picker.SelectedIndexProperty.PropertyName)
+			{
 				UpdatePicker();
+				UpdateCharacterSpacing();
+			}
+			else if (e.PropertyName == Picker.CharacterSpacingProperty.PropertyName)
+				UpdateCharacterSpacing();
 			else if (e.PropertyName == Picker.TextColorProperty.PropertyName || e.PropertyName == VisualElement.IsEnabledProperty.PropertyName)
 				UpdateTextColor();
-			else if (e.PropertyName == Picker.FontAttributesProperty.PropertyName || e.PropertyName == Picker.FontFamilyProperty.PropertyName || e.PropertyName == Picker.FontSizeProperty.PropertyName)
+			else if (e.PropertyName == Picker.FontAttributesProperty.PropertyName || e.PropertyName == Picker.FontFamilyProperty.PropertyName ||
+			         e.PropertyName == Picker.FontSizeProperty.PropertyName)
+			{
 				UpdateFont();
+			}
 		}
 
 		void OnEditing(object sender, EventArgs eventArgs)
@@ -146,9 +159,16 @@ namespace Xamarin.Forms.Platform.iOS
 		void RowsCollectionChanged(object sender, EventArgs e)
 		{
 			UpdatePicker();
+			UpdateCharacterSpacing();
 		}
 
-		protected internal virtual void UpdateFont()
+        protected void UpdateCharacterSpacing()
+        {
+	        Control.AttributedText = Control.AttributedText.AddCharacterSpacing(Control.Text, Element.CharacterSpacing);
+			Control.AttributedPlaceholder = Control.AttributedPlaceholder.AddCharacterSpacing(Element.Title, Element.CharacterSpacing);
+        }
+
+        protected internal virtual void UpdateFont()
 		{
 			Control.Font = Element.ToUIFont();
 		}
@@ -174,6 +194,8 @@ namespace Xamarin.Forms.Platform.iOS
 				var color = targetColor.IsDefault ? _defaultPlaceholderColor : targetColor;
 				Control.AttributedPlaceholder = formatted.ToAttributed(Element, color);
 			}
+
+			Control.AttributedPlaceholder = Control.AttributedPlaceholder.AddCharacterSpacing(Element.Title, Element.CharacterSpacing);
 		}
 
 
@@ -192,6 +214,7 @@ namespace Xamarin.Forms.Platform.iOS
 				return;
 
 			UpdatePickerSelectedIndex(selectedIndex);
+			UpdateCharacterSpacing();
 		}
 
 		void UpdatePickerFromModel(PickerSource s)
